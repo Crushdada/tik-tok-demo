07df240c233166d5f7fd6f8e81aad4fc16c67329
crushdada <2265997266@qq.com>
2021-04-18 19:51:31 +0800

    new commit

---
+++ src/components/index/RightBar.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="right-bar">
+    <div class="rightbar-item">
+      <div class="avatar-border">
+        <img src="../../static/img/avatar.jpg" alt="头像" />
+        <span class="iconfont icon-avatar-plus"></span>
+        <div class="icon-plus-background"></div>
+      </div>
+    </div>
+    <div class="item-icon">
+      <span class="iconfont icon-rightbar-xin">
+        <p>18.0w</p>
+      </span>
+    </div>
+    <div class="item-icon">
+      <span class="iconfont icon-rightbar-pinglun">
+        <p>18.0w</p>
+      </span>
+    </div>
+    <div class="item-icon">
+      <span class="iconfont icon-rightbar-zhuanfa">
+        <p>18.0w</p>
+      </span>
+    </div>
+    <div class="rightbar-item">
+      <div class="rightbar-music">
+        <img src="../../static/img/avatar.jpg" alt="头像" />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'RightBar',
+}
+</script>
+<style>
+.right-bar .rightbar-item {
+  width: 100%;
+  height: 50px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.avatar-border {
+  width: 52px;
+  height: 52px;
+  position: relative;
+  text-align: center;
+}
+/* 头像 */
+.right-bar .avatar-border img {
+  width: 49px;
+  height: 49px;
+  border-radius: 50%;
+  border: 1px solid rgba(248, 248, 248, 0.699);
+}
+/* 头像下的加号 */
+.avatar-border .icon-avatar-plus {
+  color: #fe2c5a;
+  position: absolute;
+  font-size: 23px;
+  top: 40px;
+  left: 0;
+  right: 0;
+  z-index: 2;
+}
+/* 在图标后面加背景颜色 */
+.avatar-border .icon-plus-background {
+  width: 12px;
+  height: 12px;
+  background-color: white;
+  position: absolute;
+  margin: 0 auto;
+  top: 46px;
+  left: 0;
+  right: 0;
+  z-index: 1;
+}
+
+/* 心、评论、转发 */
+.right-bar .item-icon {
+  width: 100%;
+  height: 70px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.item-icon .iconfont {
+  color: rgb(252, 252, 252);
+  font-size: 32px;
+  text-align: center;
+  padding-top: 12px;
+}
+.item-icon .iconfont p {
+  font-size: 7px;
+}
+.rightbar-item .rightbar-music {
+  width: 45px;
+  height: 45px;
+  margin-top: 30px;
+  background: #2e2e2e;
+  border-radius: 50%;
+  transform: scale(1.15);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #2e2e2e;
+}
+.rightbar-item .rightbar-music img {
+  width: 26px;
+  height: 26px;
+  border-radius: 50%;
+  animation: round 5s 0s linear infinite; /* 关键帧名 完成花费时间 延迟 速度曲线 播放次数 */
+}
+@keyframes round {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
---
+++ src/components/index/infoBar.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="info-bar">
+    <div class="infobar-item"><span>@Crushdada</span></div>
+    <div class="infobar-item" style="font-size: 12px">
+      <span>一个练习demo</span>
+    </div>
+    <div class="infobar-item music-item">
+      <span class="iconfont icon-yinfu"></span>
+      <div class="music-name">
+        <span data-text="身体健康">希望能开心一点</span>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'InfoBar',
+}
+</script>
+<style>
+.info-bar {
+  color: white;
+  font-size: 14px;
+  padding-left: 10px;
+  padding-bottom: 5.5px;
+}
+.info-bar .infobar-item {
+  font-size: 17px;
+  margin: 1.5px;
+}
+.iconfont .icon-yinfu {
+  margin-right: 5px;
+}
+.music-item {
+  display: flex;
+}
+.music-name {
+  width: 150px;
+  font-size: 16px;
+  white-space: nowrap; /* 强制换行 */
+  overflow: hidden;
+}
+.music-item .music-name span {
+  display: inline-block;
+  animation: scroll 2.4s linear infinite; /* 绑定的关键帧名称  完成动画花费的时间 线性速度曲线 循环播放*/
+}
+.music-item .music-name span::after {
+  content: attr(data-text);
+  margin-left: 23px; /*使用了伪元素的元素上绑定一个属性，然后就可以通过attr方法将这个属性赋给content，content就不再写死了 */
+}
+@keyframes scroll {
+  from {
+    transform: translateX(0);
+  }
+  to {
+    transform: translateX(
+      calc(-50% - 20px)
+    ); /*允许计算表达式，来动态地为元素赋值 */
+  }
+}
+</style>
